802ada4b153406f022665fa2fc7ba18628a49bb1
Matt Harvey <harveym@gmail.com>
2016-02-25 18:57:43 -0800

    Tweak.

--- flat_game/carmunk.py
+++ flat_game/carmunk.py
@@ -138,7 +138,6 @@ class GameState:
         else:
             # Higher readings are better, so return the sum.
             reward = int(self.sum_readings(readings) / 10)
-            print(reward)
         self.num_steps += 1
 
         return reward, state
